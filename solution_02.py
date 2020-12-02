51a388497666a931dd5849746f8c7e30a0c9409a
Stephen Johnson <stephen.c.johnson@gmail.com>
2020-12-02 14:40:11 -0500

    cleanup

--- solution_02.py
+++ solution_02.py
@@ -1,33 +1,29 @@
 
 
-def valid_password2(password, policy):
-    nums, letter = policy.split(" ")
-    low, high = [int(i) for i in nums.split("-")]
+def valid_password2(password, low, high, letter):
     if (password[low-1] == letter) ^ (password[high-1] == letter):
         return True
     return False
 
 
-def valid_password(password, policy):
+def valid_password(password, low, high, letter):
+    count = 0
+    count = sum([c == letter for c in password])
+    return count >= low and count <= high
+
+
+def pass_policy(line):
+    policy, password = [l.strip() for l in line.split(":")]
     nums, letter = policy.split(" ")
     low, high = [int(i) for i in nums.split("-")]
-    count = 0
-    for c in password:
-        if c == letter:
-            count += 1
-    return count >= low and count <= high
+    return password, low, high, letter
 
 
 if __name__ == "__main__":
 
     file_name = "test_02.txt"
     file_name = "input_02.txt"
-    valid = 0
-    valid2 = 0
-    for line in open(file_name):
-        policy, password = [l.strip() for l in line.split(":")]
-        if valid_password(password, policy):
-            valid += 1
-        if valid_password2(password, policy):
-            valid2 += 1
-    print(valid, valid2)
+    print(sum([valid_password(*(pass_policy(line)))
+               for line in open(file_name)]))
+    print(sum([valid_password2(*(pass_policy(line)))
+               for line in open(file_name)]))
