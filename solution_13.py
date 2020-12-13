ada178f2fa18d81c7777c8f934d388aee1d0f792
Stephen Johnson <stephen.c.johnson@gmail.com>
2020-12-13 15:28:38 -0500

    solution for day 13

---
+++ solution_13.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+if __name__ == "__main__":
+
+    file_name = "test_13.txt"
+    file_name = "input_13.txt"
+
+    f = open(file_name)
+    minutes = int(f.readline())
+    busses = f.readline().strip().split(',')
+    times = []
+    offsets = []
+    for offset, b in enumerate(busses):
+        if b.isnumeric():
+            times.append(int(b))
+            offsets.append(offset)
+    offsets = np.array(offsets)
+
+    mins = [time-minutes % time for time in times]
+
+    print(min(mins)*times[mins.index(min(mins))])  # 4207
+
+    # Yikes ... lots of struggling before remembering/finding Chinese Remainder Theorem
+    M = np.product(times)
+    bs = np.array([M//o for o in times], int)
+    bsp = np.array([pow(int(b), -1, t) for b, t in zip(bs, times)])
+    print(np.sum(-offsets*bs*bsp) % M)  # 725850285300475
