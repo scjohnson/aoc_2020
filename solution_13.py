c820f42dc4a435ba9b6665ea8fd5a76605e6731a
Stephen Johnson <stephen.c.johnson@gmail.com>
2020-12-13 15:53:39 -0500

    oh look! sympy

--- solution_13.py
+++ solution_13.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sympy.ntheory.modular import crt
 
 if __name__ == "__main__":
 
@@ -19,9 +20,11 @@ if __name__ == "__main__":
     mins = [time-minutes % time for time in times]
 
     print(min(mins)*times[mins.index(min(mins))])  # 4207
+    # If you wanna be really lazy
+    print(crt(np.array(times), (times-offsets) % times)[0])
 
     # Yikes ... lots of struggling before remembering/finding Chinese Remainder Theorem
-    M = np.product(times)
-    bs = np.array([M//o for o in times], int)
-    bsp = np.array([pow(int(b), -1, t) for b, t in zip(bs, times)])
-    print(np.sum(-offsets*bs*bsp) % M)  # 725850285300475
+    # M = np.product(times)
+    # bs = np.array([M//o for o in times], int)
+    # bsp = np.array([pow(int(b), -1, t) for b, t in zip(bs, times)])
+    # print(np.sum(-offsets*bs*bsp) % M)  # 725850285300475
