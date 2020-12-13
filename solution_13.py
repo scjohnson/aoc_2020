306797b55ced300d693d2771676350fcb3c8979b
Stephen Johnson <stephen.c.johnson@gmail.com>
2020-12-13 16:04:11 -0500

    simplify further

--- solution_13.py
+++ solution_13.py
@@ -9,21 +9,20 @@ if __name__ == "__main__":
     f = open(file_name)
     minutes = int(f.readline())
     busses = f.readline().strip().split(',')
-    times = []
-    offsets = []
+    times, offsets = np.array([], np.int), np.array([], np.int)
+    
     for offset, b in enumerate(busses):
         if b.isnumeric():
-            times.append(int(b))
-            offsets.append(offset)
-    offsets = np.array(offsets)
+            times = np.append(times, int(b))
+            offsets = np.append(offsets, offset)
+    mins = (times-minutes) % times
 
-    mins = [time-minutes % time for time in times]
-
-    print(min(mins)*times[mins.index(min(mins))])  # 4207
-    # If you wanna be really lazy
-    print(crt(np.array(times), (times-offsets) % times)[0])
-
+    print(min(mins)*times[mins == min(mins)][0])  # 4207
     # Yikes ... lots of struggling before remembering/finding Chinese Remainder Theorem
+    # If you wanna be really lazy
+    print(crt(times, (times-offsets) % times)[0])
+
+    # The old way:
     # M = np.product(times)
     # bs = np.array([M//o for o in times], int)
     # bsp = np.array([pow(int(b), -1, t) for b, t in zip(bs, times)])
