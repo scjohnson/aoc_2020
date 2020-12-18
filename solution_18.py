280e33ac2ea5aaaf5485d269ac474034025954ce
Stephen Johnson <stephen.c.johnson@gmail.com>
2020-12-18 15:15:35 -0500

    refactor

--- solution_18.py
+++ solution_18.py
@@ -5,6 +5,13 @@ class Int():
     def __init__(self, i):
         self.val = i
 
+    def line_relace(line):
+       line = line.replace("(", " ( ").replace(")", " ) ")
+       return line.replace("*", "-")
+    
+    def IntReplace(i):
+        return "Int("+i+")"
+
     def __add__(self, other):
         return Int(self.val + other.val)
 
@@ -15,6 +22,13 @@ class Int():
 class IntRev():
     def __init__(self, i):
         self.val = i
+    
+    def line_relace(line):
+       line = line.replace("(", " ( ").replace(")", " ) ")
+       return line.replace("*", "-").replace("+", "*").replace("-", "+") 
+
+    def IntReplace(i):
+        return "IntRev("+i+")"
 
     def __add__(self, other):
         return IntRev(self.val * other.val)
@@ -23,21 +37,14 @@ class IntRev():
         return IntRev(self.val + other.val)
 
 
-def evaluate(line, rev=False):
-    if rev:
-        line = line.replace("*", "-").replace("+", "*").replace("-", "+")
-    else:
-        line = line.replace("*", "-")
-    line = line.replace("(", " ( ").replace(")", " ) ")
+def evaluate(line, IntClass=Int):
+    line = IntClass.line_relace(line)
 
     splits = line.split(" ")
     for i, split in enumerate(splits):
         try:
             j = int(split)
-            if rev:
-                splits[i] = "IntRev("+split+")"
-            else:
-                splits[i] = "Int("+split+")"
+            splits[i] = IntClass.IntReplace(split)
         except:
             pass
     line = " ".join(splits)
@@ -52,5 +59,5 @@ if __name__ == "__main__":
 
     print(sum(evaluate(line.strip())
               for line in open(file_name)))  # 50956598240016
-    print(sum(evaluate(line.strip(), True)
+    print(sum(evaluate(line.strip(), IntRev)
               for line in open(file_name)))  # 535809575344339
