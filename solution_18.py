ba6cdbf6c7278257dbac6f256771677e048d79d3
Stephen Johnson <stephen.c.johnson@gmail.com>
2020-12-18 09:31:38 -0500

    simplify

--- solution_18.py
+++ solution_18.py
@@ -1,4 +1,6 @@
 import re
+
+
 class Int():
     def __init__(self, i):
         self.val = i
@@ -21,31 +23,21 @@ class IntRev():
         return IntRev(self.val + other.val)
 
 
-def evaluate(line):
-    line = line.replace("*", "-")
+def evaluate(line, rev=False):
+    if rev:
+        line = line.replace("*", "-").replace("+", "*").replace("-", "+")
+    else:
+        line = line.replace("*", "-")
     line = line.replace("(", " ( ").replace(")", " ) ")
 
     splits = line.split(" ")
     for i, split in enumerate(splits):
         try:
             j = int(split)
-            splits[i] = "Int("+split+")"
-        except:
-            pass
-    line = " ".join(splits)
-
-    return eval(line).val
-
-
-def evaluate_rev(line):
-    line = line.replace("*", "-").replace("+", "*").replace("-", "+")
-    line = line.replace("(", " ( ").replace(")", " ) ")
-
-    splits = line.split(" ")
-    for i, split in enumerate(splits):
-        try:
-            j = int(split)
-            splits[i] = "IntRev("+split+")"
+            if rev:
+                splits[i] = "IntRev("+split+")"
+            else:
+                splits[i] = "Int("+split+")"
         except:
             pass
     line = " ".join(splits)
@@ -58,5 +50,7 @@ if __name__ == "__main__":
     file_name = "test_18a.txt"
     file_name = "input_18.txt"
 
-    print(sum(evaluate(line.strip()) for line in open(file_name)))  # 50956598240016
-    print(sum(evaluate_rev(line.strip()) for line in open(file_name))) # 535809575344339
+    print(sum(evaluate(line.strip())
+              for line in open(file_name)))  # 50956598240016
+    print(sum(evaluate(line.strip(), True)
+              for line in open(file_name)))  # 535809575344339
