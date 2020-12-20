4a5e70dc0c76df69f5566329d179fbb7b30b6d98
Stephen Johnson <stephen.c.johnson@gmail.com>
2020-12-20 17:46:06 -0500

    both parts working, but yuck

--- solution_20.py
+++ solution_20.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools
+import scipy.signal
 
 
 def read_tiles(file_name, array_size):
@@ -24,56 +25,134 @@ def read_tiles(file_name, array_size):
 
     return tiles
 
+
+def try_fit(big_image, big_i, big_j, tile):
+    if big_i != 0:
+        y_start = big_j*9
+        y_stop = y_start+10
+        x_start = big_i*10-(big_i-1)-1
+        to_match = big_image[y_start:y_stop, x_start]
+        for i in range(4):
+            if np.all(to_match == tile[:, 0]):
+                big_image[y_start:y_stop, x_start:x_start+10] = tile
+                return True
+            tile = np.rot90(tile)
+        tile = np.fliplr(tile)
+        for i in range(4):
+            if np.all(to_match == tile[:, 0]):
+                big_image[y_start:y_stop, x_start:x_start+10] = tile
+                return True
+            tile = np.rot90(tile)
+    else:
+        y_start = big_j*9
+        x_start = 0
+        x_stop = 10
+        to_match = big_image[y_start, x_start:x_stop]
+        for i in range(4):
+            if np.all(to_match == tile[0, :]):
+                big_image[y_start:y_start+10, x_start:x_stop] = tile
+                return True
+            tile = np.rot90(tile)
+        tile = np.fliplr(tile)
+        for i in range(4):
+            if np.all(to_match == tile[0, :]):
+                big_image[y_start:y_start+10, x_start:x_stop] = tile
+                return True
+            tile = np.rot90(tile)
+    return False
+
+
 def connect(edge, tile2):
-    # print("e: ", edge)
-    # print("tile: ", tile2)
-    if np.all(edge == tile2[0,:]):
+    if np.all(edge == tile2[0, :]):
         return True
-    if np.all(edge == tile2[-1,:]):
+    if np.all(edge == tile2[-1, :]):
         return True
-    if np.all(edge == tile2[:,0]):
+    if np.all(edge == tile2[:, 0]):
         return True
-    if np.all(edge == tile2[:,-1]):
+    if np.all(edge == tile2[:, -1]):
         return True
-    if np.all(edge == np.flip(tile2[0,:])):
+    if np.all(edge == np.flip(tile2[0, :])):
         return True
-    if np.all(edge == np.flip(tile2[-1,:])):
+    if np.all(edge == np.flip(tile2[-1, :])):
         return True
     if np.all(edge == np.flip(tile2[:, 0])):
         return True
-    if np.all(edge == np.flip(tile2[:,-1])):
+    if np.all(edge == np.flip(tile2[:, -1])):
         return True
+
 
 if __name__ == "__main__":
 
     file_name = "test_20.txt"
     array_size = 10
     image_size = 3
-    # file_name = "input_20.txt"
-    # image_size = 12
+    file_name = "input_20.txt"
+    image_size = 12
 
     tiles = read_tiles(file_name, array_size)
     mult = 1
+    starting_corner = 0
     for tile_id1, tile1 in tiles.items():
-        no_matches = [0,0,0,0]
+        no_matches = [0, 0, 0, 0]
         for tile_id2, tile2 in tiles.items():
-            # print("tile1: ", tile1)
-            # print("tile2: ", tile2)
-            # print(tile1.shape, tile2.shape)
             if tile_id1 != tile_id2:
-                if connect(tile1[0,:], tile2):
+                if connect(tile1[0, :], tile2):
                     no_matches[0] = 1
-                if connect(tile1[-1,:], tile2):
+                if connect(tile1[-1, :], tile2):
                     no_matches[1] = 1
-                if connect(tile1[:,0], tile2):
+                if connect(tile1[:, 0], tile2):
                     no_matches[2] = 1
-                if connect(tile1[:,-1], tile2):
+                if connect(tile1[:, -1], tile2):
                     no_matches[3] = 1
         if sum(no_matches) == 2:
-            print(tile_id1)
             mult *= tile_id1
-        print(sum(no_matches))
-    print(mult) # 18449208814679
+            if (no_matches[1] == 1 and no_matches[3] == 1):
+                starting_corner = tile_id1
+    print("part 1: ", mult)  # 18449208814679
 
+    big_i, big_j = 1, 0
+    big_image_size = image_size*array_size-(image_size-1)
+    big_image = np.zeros([big_image_size, big_image_size], np.int)
+    big_image[:array_size, :array_size] = tiles[starting_corner]
+    used_tiles = [starting_corner]
+    while len(used_tiles) != len(tiles):
+        for tile_id, tile in tiles.items():
+            if tile_id in used_tiles:
+                continue
+            if try_fit(big_image, big_i, big_j, tile):
+                used_tiles.append(tile_id)
+                big_i += 1
+                if big_i == image_size:
+                    big_j += 1
+                    big_i = 0
 
+    # new_big_image = np.zeros(
+    #    [big_image.shape[0]-image_size-1, big_image.shape[1]-image_size-1], np.int)
+    for big_i in range(big_image.shape[0]-1, -1, -9):
+        big_image = np.delete(big_image, big_i, 0)
+        big_image = np.delete(big_image, big_i, 1)
 
+    monster = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                        [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+                            1, 1, 0, 0, 0, 0, 1, 1, 1],
+                        [0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0]], np.int)
+    # for small one:
+    # big_image = np.fliplr(big_image)
+
+    big_image = np.fliplr(big_image)
+    big_image = np.rot90(big_image)
+    big_image = np.rot90(big_image)
+    # big_image = np.rot90(big_image)
+    # print(big_image)
+    num_monsters = 0
+    for i in range(0, big_image.shape[0]-monster.shape[0]):
+        for j in range(0, big_image.shape[1]-monster.shape[1]):
+            sub_image = big_image[i:i+monster.shape[0], j:j+monster.shape[1]] 
+            if np.all(sub_image[monster == 1] == 1):
+                num_monsters += 1
+    if (num_monsters > 0):
+        print("found monsters")
+    print(np.sum(big_image)-num_monsters*np.sum(monster))
+
+    # hits = scipy.signal.convolve2d(big_image, monster)
+    # print(sum(hits))
