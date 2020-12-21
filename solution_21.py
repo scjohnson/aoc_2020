29985043207bf5ef912ce762c2d93cf1fa2b27ba
Stephen Johnson <stephen.c.johnson@gmail.com>
2020-12-21 11:14:41 -0500

    cleanup

--- solution_21.py
+++ solution_21.py
@@ -5,14 +5,6 @@ class Recipe:
         self.ingredients = ing.split(" ")
         self.alergens = aler[9:-1].split(", ")
 
-    def delete_ingredient(self, ingredient):
-        if ingredient in self.ingredients:
-            self.ingredients.remove(ingredient)
-
-    def delete_alergen(self, alergen):
-        if alergen in self.alergens:
-            self.alergens.remove(alergen)
-
 
 if __name__ == "__main__":
 
@@ -20,17 +12,13 @@ if __name__ == "__main__":
     file_name = "input_21.txt"
     recipes = [Recipe(line.strip()) for line in open(file_name)]
 
-    alergens = [alergen for recipe in recipes for alergen in recipe.alergens]
-    ingredients = [
-        ingredient for recipe in recipes for ingredient in recipe.ingredients]
-    alergens = list(set(alergens))
-    orig_alergens = alergens.copy()
-    ingredients = list(set(ingredients))
+    alergens = list(
+        set([alergen for recipe in recipes for alergen in recipe.alergens]))
+    ingredients = list(set([
+        ingredient for recipe in recipes for ingredient in recipe.ingredients]))
 
     alergic_ingredients = {}
-
-    while len(alergens) != 0:
-        to_delete = []
+    while len(alergic_ingredients) != len(alergens):
         for alergen in alergens:
             potentials = ingredients.copy()
             for recipe in recipes:
@@ -40,11 +28,10 @@ if __name__ == "__main__":
             if len(potentials) == 1:
                 alergic_ingredients[alergen] = potentials[0]
                 for recipe in recipes:
-                    recipe.delete_ingredient(potentials[0])
-                    recipe.delete_alergen(alergen)
-                to_delete.append(alergen)
-        for td in to_delete:
-            alergens.remove(td)
+                    if potentials[0] in recipe.ingredients:
+                        recipe.ingredients.remove(potentials[0])
+                    if alergen in recipe.alergens:
+                        recipe.alergens.remove(alergen)
 
     leftovers = {}
     for recipe in recipes:
@@ -53,11 +40,10 @@ if __name__ == "__main__":
                 leftovers[ing] += 1
             else:
                 leftovers[ing] = 1
-
     print("part 1: ", sum([n for _, n in leftovers.items()]))
 
     s = []
-    for alergen in sorted(orig_alergens):
+    for alergen in sorted(alergens):
         if alergen in alergic_ingredients:
             s.append(alergic_ingredients[alergen])
     # dhfng,pgblcd,xhkdc,ghlzj,dstct,nqbnmzx,ntggc,znrzgs
