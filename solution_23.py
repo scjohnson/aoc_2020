44e86e92b4edefd09714eed8fc26dfa73e2e811e
Stephen Johnson <stephen.c.johnson@gmail.com>
2020-12-23 14:39:40 -0500

    simplify

--- solution_23.py
+++ solution_23.py
@@ -1,38 +1,29 @@
-from collections import deque
 from tqdm import tqdm
-
-
-def play(cups):
-    l = len(cups)
-    t = cups.popleft()
-    hold_out = [cups.popleft() for _ in range(3)]
-    dest_cup = (t-1) % l
-    while dest_cup in hold_out:
-        dest_cup = (dest_cup-1) % l
-    dest_index = cups.index(dest_cup) + 1
-    cups.insert(dest_index, hold_out[0])
-    cups.insert(dest_index+1, hold_out[1])
-    cups.insert(dest_index+2, hold_out[2])
-    cups.append(t)
-    return cups
 
 
 def dplay(cups, dest):
     c1 = cups[dest]
-    c2 = cups[cups[dest]]
-    c3 = cups[cups[cups[dest]]]
-    c4 = cups[cups[cups[cups[dest]]]]
+    c2 = cups[c1]
+    c3 = cups[c2]
+    cups[dest] = cups[c3]
 
     search_val = (dest-1) % len(cups)
     while search_val in [c1, c2, c3]:
         search_val = (search_val-1) % len(cups)
+
     cups[c3] = cups[search_val]
     cups[search_val] = c1
-
-    cups[dest] = c4
-    dest = c4
+    dest = cups[dest]
 
     return cups, dest
+
+
+def list_to_dict(l):
+    dcups = {}
+    for i in range(len(cups)-1):
+        dcups[cups[i]-1] = cups[i+1]-1
+    dcups[cups[-1]-1] = cups[0]-1
+    return dcups
 
 
 if __name__ == "__main__":
@@ -40,21 +31,20 @@ if __name__ == "__main__":
     # cups = [3, 8, 9, 1, 2, 5, 4, 6, 7]
     cups = [8, 5, 3, 1, 9, 2, 6, 4, 7]
 
-    cupsd = deque([i-1 for i in cups])
-    for _ in range(100):
-        cupsd = play(cupsd)
-    results = [cupsd.popleft()+1 for i in range(len(cups))]  # 97624853
-    results = results[results.index(1) + 1:] + results[:results.index(1)]
-    print("".join([str(s) for s in results]))
-
-    cups = cups + list(range(10, 1000001))
-
-    dcups = {}
-    for i in range(len(cups)-1):
-        dcups[cups[i]-1] = cups[i+1]-1
-    dcups[cups[-1]-1] = cups[0]-1
-
+    dcups = list_to_dict(cups)
     dest = cups[0]-1
-    for _ in tqdm(range(10000000)):
+    for _ in tqdm(range(100)):
         dcups, dest = dplay(dcups, dest)
+
+    res = [0]
+    for _ in range(8):
+        res.append(dcups[res[-1]])
+    print("".join([str(s+1) for s in res[1:]]))  # 97624853
+
+    cups=cups + list(range(10, 1000001))
+
+    dcups=list_to_dict(cups)
+    dest=cups[0]-1
+    for _ in tqdm(range(10000000)):
+        dcups, dest=dplay(dcups, dest)
     print((dcups[0]+1)*(dcups[dcups[0]]+1))  # 664642452305
